9c1068a4235f76b349dc2c5c0d156079bc17c202
hack <hack@stsci.edu>
2004-06-29 18:38:24 +0000

    Created new setup.py file for 'pydrizzle' and 'traits102' for distribution. WJH
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/pydrizzle@41 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -3,17 +3,73 @@ import numarray
 from numarray.numarrayext import NumarrayExtension
 import sys
 
-if not hasattr(sys, 'version_info') or sys.version_info < (2,2,0,'alpha',0):
-    raise SystemExit, "Python 2.2 or later required to build numarray."
+if not hasattr(sys, 'version_info') or sys.version_info < (2,3,0,'alpha',0):
+    raise SystemExit, "Python 2.3 or later required to build imagestats."
 
-setup(name = "arrdriz",
-      version = "0.5",
-      description = "",
-      packages=[""],
-      package_dir={"":""},
-      ext_modules=[NumarrayExtension("arrdriz",['arrdrizmodule.c','tdriz.c','tblot.c','drutil.c','doblot.c',
-                                                'drcall.c','inter2d.c','bieval.c'],
-                   include_dirs=["/usr/ra/f2c"],
-                   library_dirs=["/usr/ra/f2c"],
+def dolocal():
+    """Adds a command line option --local=<install-dir> which is an abbreviation for
+    'put all of pydrizzle in <install-dir>/pydrizzle'."""
+    if "--help" in sys.argv:
+        print >>sys.stderr
+        print >>sys.stderr, " options:"
+        print >>sys.stderr, "--local=<install-dir>    same as --install-lib=<install-dir>"
+    for a in sys.argv:
+        if a.startswith("--local="):
+            dir = a.split("=")[1]
+            sys.argv.extend([
+                "--install-lib="+dir,
+                ])
+            sys.argv.remove(a)
+
+def getF2CDirs():
+    """ Defines the location of the F2C include and library directories. """
+
+    platform = sys.platform[:5]
+    if platform == 'linux':
+        f2c_include  = "/data/chulak1/local/include"
+        f2c_lib      = "/data/chulak1/local/lib"
+    elif platform == 'sunos':
+        f2c_include  = "/usr/ra/f2c"
+        f2c_lib      = "/usr/ra/f2c"
+    else:
+        print 'ERROR: Unsupported platform: ',sys.platform
+        print 'ERROR: No supported version of F2C available!'
+        raise ValueError
+
+    return f2c_include,f2c_lib
+
+def getExtensions(f2cdirs):
+    ext = [NumarrayExtension("arrdriz",['src/arrdrizmodule.c','src/tdriz.c','src/tblot.c',
+                                'src/drutil.c','src/doblot.c','src/drcall.c',
+                                'src/inter2d.c','src/bieval.c'],
+                   include_dirs=[f2cdirs[0]],
+                   library_dirs=[f2cdirs[1]],
                    libraries=['f2c','m'])]
-      )
+
+    return ext
+
+
+def dosetup(ext):
+    r = setup(name = "pydrizzle",
+              version = "5.0.17",
+              description = "Geometrically correct and combine images using Drizzle",
+              author = "Warren Hack",
+              author_email = "help@stsci.edu",
+              license = "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
+              platforms = ["Linux","Solaris"],
+              packages=['pydrizzle','pydrizzle/traits102'],
+              package_dir={'pydrizzle':'lib','pydrizzle/traits102':'traits102'},
+              ext_modules=ext)
+    return r
+
+
+def main():
+    args = sys.argv
+    dolocal()
+    f2cdirs = getF2CDirs()
+    ext = getExtensions(f2cdirs)
+    dosetup(ext)
+
+
+if __name__ == "__main__":
+    main()
