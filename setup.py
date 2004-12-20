d92649a6af511bed3ce30c430ad7dc1515b5e206
iraf <iraf@stsci.edu>
2004-12-20 20:47:58 +0000

    Checking in released version of setup
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/pydrizzle@230 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup, Extension
 import numarray
 from numarray.numarrayext import NumarrayExtension
-import sys, string, os
+import sys, string, os.path, shutil
 
 if not hasattr(sys, 'version_info') or sys.version_info < (2,3,0,'alpha',0):
-    raise SystemExit, "Python 2.3 or later required to build imagestats."
+    raise SystemExit, "Python 2.3 or later required to build pydrizzle."
+
+if numarray.__version__ < "1.1":
+    raise SystemExit, "Numarray 1.1 or later required to build pydrizzle."
+ver = sys.version_info
+python_exec = 'python' + str(ver[0]) + '.' + str(ver[1])
 
 def dolocal():
     """Adds a command line option --local=<install-dir> which is an abbreviation for
@@ -15,40 +20,67 @@ def dolocal():
         print >>sys.stderr, "--local=<install-dir>    same as --install-lib=<install-dir>"
     for a in sys.argv:
         if a.startswith("--local="):
-            dir = a.split("=")[1]
+            dir =  os.path.abspath(a.split("=")[1])
             sys.argv.extend([
                 "--install-lib="+dir,
+		"--install-data="+os.path.join(dir,"pydrizzle")
                 ])
             sys.argv.remove(a)
 
 def getF2CDirs(args):
     """ Defines the location of the F2C include and library directories. """
+    if "--help" in sys.argv:
+        print >>sys.stderr
+        print >>sys.stderr, " options:"
+        print >>sys.stderr, "--with-f2c=<f2c-dir> "
     for a in args:
         if string.find(a, '--with-f2c=') != -1:
             f2cdir = string.split(a, '=')[1]
             sys.argv.remove(a)
-            if os.path.isfile(os.path.join(f2cdir, 'f2c.h')):
+            if os.path.exists(os.path.join(f2cdir, 'f2c.h')):
                 f2c_include = f2cdir
-            elif os.path.isfile(os.path.join(f2cdir, 'include','f2c.h')):
-                f2c_include = os.path.join(f2cdir, 'include','f2c.h')
+            elif os.path.exists(os.path.join(f2cdir, 'include','f2c.h')):
+                f2c_include = os.path.join(f2cdir, 'include')
             else:
-                print "File f2c.h not found.\n"
-                sys.exit(1)
-            if os.path.isfile(os.path.join(f2cdir, 'libf2c.a')):
+                raise SystemExit, "f2c.h needed to build pydrizzle."
+            if os.path.exists(os.path.join(f2cdir, 'libf2c.a')):
                 f2c_lib = f2cdir
-            elif os.path.isfile(os.path.join(f2cdir, 'include','libf2c.a')):
-                f2c_include = os.path.join(f2cdir, 'lib','libf2c.a')
+            elif os.path.exists(os.path.join(f2cdir, 'lib','libf2c.a')):
+                f2c_lib = os.path.join(f2cdir, 'lib')
             else:
-                print "Library libf2c.a not found.\n"
-                sys.exit(1)
-
+                raise SystemExit, "libf2c needed to build pydrizzle."
     return f2c_include,f2c_lib
 
-def getExtensions(f2cdirs):
+def getDataDir(args):
+    for a in args:
+        if string.find(a, '--home=') == 0:
+            dir = string.split(a, '=')[1]
+            data_dir = os.path.join(dir, 'lib/python/pydrizzle')
+        elif string.find(a, '--prefix=') == 0:
+            dir = string.split(a, '=')[1]
+            data_dir = os.path.join(dir, 'lib', python_exec, 'site-packages/pydrizzle')
+        elif a.startswith('--install-data='):
+            dir = string.split(a, '=')[1]
+            data_dir = dir
+        else:
+            data_dir = os.path.join(sys.prefix, 'lib', python_exec, 'site-packages/pydrizzle')
+    return data_dir
+
+
+def getExtensions(f2cdirs,args):
+    numarrayIncludeDir = './'
+    for a in args:
+        if a.startswith('--home='):
+            numarrayIncludeDir = os.path.abspath(os.path.join(a.split('=')[1], 'include', 'python', 'numarray'))
+        elif a.startswith('--prefix='):
+            numarrayIncludeDir = os.path.abspath(os.path.join(a.split('=')[1], 'include','python2.3', 'numarray'))
+        elif a.startswith('--local='):
+            numarrayIncludeDir = os.path.abspath(a.split('=')[1])
+
     ext = [NumarrayExtension("pydrizzle/arrdriz",['src/arrdrizmodule.c','src/tdriz.c','src/tblot.c',
                                 'src/drutil.c','src/doblot.c','src/drcall.c',
                                 'src/inter2d.c','src/bieval.c'],
-                   include_dirs=[f2cdirs[0]],
+                   include_dirs=[numarrayIncludeDir, f2cdirs[0]],
                    library_dirs=[f2cdirs[1]],
                    libraries=['f2c','m'])]
 
@@ -62,20 +94,31 @@ def dosetup(ext):
               author = "Warren Hack",
               author_email = "help@stsci.edu",
               license = "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
-              platforms = ["Linux","Solaris"],
+              platforms = ["Linux","Solaris", "MacOS X"],
               packages=['pydrizzle','pydrizzle/traits102'],
               package_dir={'pydrizzle':'lib','pydrizzle/traits102':'traits102'},
               ext_modules=ext)
     return r
 
+def copy_doc(data_dir, args):
+    if 'install' in args:
+        doc_dir = os.path.join(data_dir,'doc')
+        if os.path.exists(doc_dir):
+	    try:
+                shutil.rmtree(doc_dir)
+            except:
+                print "Error removing doc directory\n"
+	shutil.copytree('doc', doc_dir)
+
 
 def main():
     args = sys.argv
     dolocal()
+    data_dir = getDataDir(args)
     f2cdirs = getF2CDirs(args)
-    ext = getExtensions(f2cdirs)
+    ext = getExtensions(f2cdirs,args)
     dosetup(ext)
-
+    copy_doc(data_dir, args)
 
 if __name__ == "__main__":
     main()
