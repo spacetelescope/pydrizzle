7c17caef04228a13b4caa8f91a294fd06fc6001d
hack <hack@stsci.edu>
2005-04-04 20:34:25 +0000

    Testing merge functions in CVS. No actual functional changes. WJH
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/pydrizzle@333 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/buildmask.py
+++ lib/buildmask.py
@@ -29,6 +29,8 @@ Functions to build mask files for PyDrizzle.
 #                   added output filename as input parameter to 'buildMask'
 #                   functions. WJH
 #
+# Test
+#
 import string,os,types
 
 import fileutil
